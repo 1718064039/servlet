83056a4646007a7c2029a12b7af97649ccd303e7
1718064039 <63086516+1718064039@users.noreply.github.com>
2020-04-19 03:07:17 +0800

    1718064039
    网络1701 蔡星佑 1718064039

---
+++ two/src/Servlet3.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.Cookie;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class servlet3
+ */
+@WebServlet("/Servlet3")
+public class Servlet3 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet3() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter();
+        Cookie ck = new Cookie("account", "");
+        Cookie ck1=new Cookie("password","");
+        ck1.setMaxAge(0);
+        ck.setMaxAge(0);
+        response.addCookie(ck);
+        response.addCookie(ck1);
+        out.print("<h1 align=\"center\">ÄãÒÑ¾­³É¹¦×¢Ïú!</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
